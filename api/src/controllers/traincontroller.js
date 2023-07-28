2675efa73b45e28ff1f04778004be9eada37cf09
Pranjal <pranjalsingh531@gmail.com>
2023-07-28 17:05:15 +0530

    backend done

--- api/src/controllers/traincontroller.js
+++ api/src/controllers/traincontroller.js
@@ -29,7 +29,7 @@ const getTrains = async  (req,res) =>{
     // console.log(trainData);
 
     //30min train ignore      //prev train ignore
-    const finalData = []
+    const trains = []
 
     const currTime = new Date();
     const hours = currTime.getHours();
@@ -40,29 +40,43 @@ const getTrains = async  (req,res) =>{
     trainData.forEach((ele) => {
         let depTime = ele.departureTime.Hours * 60 * 60 + ele.departureTime.Minutes *60 + ele.departureTime.Seconds;
         if(findTimeDiff(currentTime, depTime) > 30){
-            finalData.push(ele);
+            trains.push(ele);
         }
     });
   
 
-    //sort on price
-    finalData.sort((a,b) => {
-        return (a.price.sleeper + a.price.AC) - (b.price.sleeper + b.price.AC) ;
-    })
+    //sort on price   //descending sort on tickets available   //descending sort on departure time
 
-    //descending sort on tickets 
-    // finalData.sort((a,b) => {
-    //     return (b.seatsAvailable.sleeper + b.seatsAvailable.AC) - (a.seatsAvailable.sleeper + a.seatsAvailable.AC);
-    // })
+    const now = new Date();
+    const nowInMinutes = now.getHours() * 60 + now.getMinutes();
+    const nowInMilliseconds = nowInMinutes * 60 * 1000;
 
-    //descending order of departure time
+    const allowedTimeWindowInMilliseconds = 12 * 60 * 60 * 1000;
 
-    // finalData.sort((a,b) => {
-    //     let aTime = a.departureTime.Hours * 60 * 60 + a.departureTime.Minutes *60 + a.departureTime.Seconds;
-    //     let bTime = b.departureTime.Hours * 60 * 60 + b.departureTime.Minutes *60 + b.departureTime.Seconds;
-    //     return bTime - aTime;
-    // })
-    console.log(finalData);
+
+    const trainsAfterAllowedTimeWindow = trains.filter(train => {
+        const departureTime = train.departureTime.Hours * 60 * 60 * 1000 + train.departureTime.Minutes * 60 * 1000 + train.departureTime.Seconds * 1000;
+    
+        return departureTime - nowInMilliseconds < allowedTimeWindowInMilliseconds;
+    });
+
+    const trainsSorted = trainsAfterAllowedTimeWindow.sort((a, b) => {
+        if (a.price.sleeper + a.price.AC !== b.price.AC + b.price.sleeper) {
+            return a.price.sleeper + a.price.AC - b.price.sleeper - b.price.AC;
+        } 
+        if (a.seatsAvailable.sleeper + a.seatsAvailable.AC !== b.seatsAvailable.sleeper + b.seatsAvailable.AC) {
+            return b.seatsAvailable.sleeper + b.seatsAvailable.AC - a.seatsAvailable.sleeper + a.seatsAvailable.AC;
+        }
+        const aDepartureTime = new Date(a.departureTime.Hours * 60 * 60 * 1000 + a.departureTime.Minutes * 60 * 1000 + a.departureTime.Seconds * 1000);
+        const bDepartureTime = new Date(b.departureTime.Hours * 60 * 60 * 1000 + b.departureTime.Minutes * 60 * 1000 + b.departureTime.Seconds * 1000);
+
+        const aDepartureTimeInMinutes = aDepartureTime.getHours() * 60 + aDepartureTime.getMinutes() + a.delayedBy;
+        const bDepartureTimeInMinutes = bDepartureTime.getHours() * 60 + bDepartureTime.getMinutes() + b.delayedBy;
+        return aDepartureTimeInMinutes - bDepartureTimeInMinutes;
+    });
+
+    res.status(200).json(trainsSorted);
+
 
 }
 
